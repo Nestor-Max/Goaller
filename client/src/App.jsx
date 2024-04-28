ff66ea22833693db52e1be3b89b74c42f47d6f51
Nnabude Nestor <nestornnabude@gmail.com>
2024-04-28 11:06:03 +0100

    setting up header links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
 //import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import About from './pages/About';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
 
 export default function App() {
 	return (
-		<div>
-			<h1 className="text-red-500">APP</h1>
-		</div>
+		<BrowserRouter>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/profile" element={<Profile />} />
+				<Route path="/sign-in" element={<SignIn />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
